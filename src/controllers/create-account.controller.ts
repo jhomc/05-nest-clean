c1f6941656838344ccd8c87a0887e910ece8f8a2
Jhonata Costa <jhomcosta@gmail.com>
2023-10-12 15:20:27 -0300

    feat: zod validation

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,13 +1,31 @@
-import { ConflictException, Body, Controller, Post } from '@nestjs/common'
+import {
+  ConflictException,
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@Body() body: any) {
-    const { name, email, password } = body
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = createAccountBodySchema.parse(body)
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
@@ -19,11 +37,13 @@ export class CreateAccountController {
       throw new ConflictException('User with same email already exists')
     }
 
+    const hashedPassword = await hash(password, 8)
+
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
       },
     })
   }
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,25 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value)
+    } catch (err) {
+      if (err instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validations failed',
+          statusCode: 400,
+          errors: fromZodError(err),
+        })
+      }
+
+      throw new BadRequestException('Validations failed')
+    }
+
+    return value
+  }
+}
