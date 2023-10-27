984ddee6d5ea737fe7b988e742d5fb3795c795f7
Jhonata Costa <jhomcosta@gmail.com>
2023-10-27 16:50:51 -0300

    feat: questions tests

---
+++ src/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -0,0 +1,67 @@
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+
+describe('Create question (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET]/questions', async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: await hash('123456', 8),
+      },
+    })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    await prisma.question.createMany({
+      data: [
+        {
+          title: 'Question Example',
+          content: 'question example content',
+          slug: 'question-example',
+          authorId: user.id,
+        },
+        {
+          title: 'Question Example 2',
+          content: 'question example content 2',
+          slug: 'question-example-2',
+          authorId: user.id,
+        },
+      ],
+    })
+
+    const response = await request(app.getHttpServer())
+      .get('/questions')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      questions: [
+        expect.objectContaining({ title: 'Question Example' }),
+        expect.objectContaining({ title: 'Question Example 2' }),
+      ],
+    })
+  })
+})
